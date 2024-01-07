0474db8ee9b09be50573ca8e8d19a195ab5c780f
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2024-01-07 16:57:35 -0300

    Delete Projeto Trello directory

--- Projeto Trello/trelloso_fastapi/app/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.routers.auth import router as router_auth
-from app.routers.tag import router as router_tag
-from app.routers.user import router as router_user
-from app.routers.board import router as router_board
-from app.routers.list import router as router_list
-from app.routers.card import router as router_card
-from app.routers.card_comment import router as router_cardcomment
-from app.routers.card_tag import router as router_cardtag
-from app.routers.card_member import router as router_cardmember
