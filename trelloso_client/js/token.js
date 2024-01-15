07cfd4be476f6ca3baf857e76e04186311cc0415
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2024-01-15 12:15:48 -0300

    Delete trelloso_client directory

--- trelloso_client/js/token.js
+++
@@ -1,9 +0,0 @@
-function saveToken(token) {
-    localStorage.setItem("token", `${token.token_type} ${token.access_token}`);
-}
-
-function getToken(token) {
-    return localStorage.getItem("token");
-}
-
-export default {saveToken, getToken};
